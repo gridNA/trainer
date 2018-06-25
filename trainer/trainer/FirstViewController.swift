162981f55289baa40637ad6895de5f2bdcfe28ed
Kateryna Gridina <kateryna.gridina@zalando.de>
2018-06-25 14:13:24 +0100

    add initial project

---
+++ trainer/trainer/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  trainer
+//
+//  Created by Kateryna Gridina on 25.06.18.
+//  Copyright © 2018 zalando. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
